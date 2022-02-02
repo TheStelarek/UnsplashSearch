94fe0d896f81e7af624caaf1818f9482cc52f57a
Dennis <thestelarek97@gmail.com>
2022-02-02 21:36:46 +0100

    add button, navbar and photocard

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="nav">
+    <router-link to="/" v-if="this.$route.name === 'Home'">Home</router-link>
+    <router-link to="/" v-if="this.$route.name !== 'Home'">Back</router-link>
+  </div>
+</template>
+
+<style lang="scss">
+#nav {
+  padding: 30px;
+  display: flex;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/components/PhotoCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <router-link :to="{ name: 'photo-show', params: { id: photo.id } }">
+    <img :src="photo.urls.small" class="photo-container" />
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    photo: Object
+  }
+}
+</script>
+
+<style>
+@media (max-width: 600px) {
+  .photo-container {
+    max-width: 350px;
+    width: 100%;
+  }
+}
+
+@media (max-width: 400px) {
+  .photo-container {
+    max-width: 250px;
+    width: 100%;
+  }
+}
+</style>
